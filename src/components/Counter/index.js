2e31147c08a8e8d9e70c6ae900075410132421fd
Nagaphaneendra <Nagaphaneendra6240@outlook.com>
2023-03-10 10:14:13 +0000

    Counter App using React Class Component and State

---
+++ src/components/Counter/index.js
@@ -0,0 +1,34 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class Counter extends Component {
+  state = {count: 0}
+  onIncrement = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+  onDecrement = () => {
+    this.setState(prevState => ({count: prevState.count - 1}))
+  }
+  render() {
+    const {count} = this.state
+    return (
+      <div className="banner-container">
+        <div className="container">
+          <h1 className="heading">Counter</h1>
+          <h1 className="count">{count}</h1>
+          <div>
+            <button className="button" onClick={this.onIncrement}>
+              Increase
+            </button>
+            <button className="button" onClick={this.onDecrement}>
+              Decrease
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Counter
